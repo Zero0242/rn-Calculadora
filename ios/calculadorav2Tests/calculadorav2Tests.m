7058b90182fec61542bb862c6d4574f2e5e49a04
Zero0242 <pedrocorream66@gmail.com>
2024-03-06 02:28:08 -0300

    V2: inicio de la version 2
    > Nuevo prroyecto react native

--- ios/calculadorav2Tests/calculadorav2Tests.m
+++ ios/calculadorav2Tests/calculadorav2Tests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RNCalculadoraTests : XCTestCase
+@interface calculadorav2Tests : XCTestCase
 
 @end
 
-@implementation RNCalculadoraTests
+@implementation calculadorav2Tests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
