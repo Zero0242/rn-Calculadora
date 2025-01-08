57b8cd240c2533070dcfdcccc31a0adedd26f31a
Zero0242 <pedrocorream66@gmail.com>
2025-01-08 15:01:14 -0300

    [BP]: eliminacion de boiler

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function CalculadoraApp() {
+    return (
+        <View>
+            <Text style={{ fontSize: 50 }}>Calculadora App</Text>
+        </View>
+    )
+}
