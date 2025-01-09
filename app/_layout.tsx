c69672999d40c8603761e10f408865dfa79eb0ae
Zero0242 <pedrocorream66@gmail.com>
2025-01-09 13:32:03 -0300

    [THEME]: configuración del tema
    > registro

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Slot } from 'expo-router'
+import { View } from 'react-native'
+import React, { useEffect } from 'react'
+import { Slot, SplashScreen } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
+import { useFonts } from 'expo-font';
+import { Colors } from '@/constants/Colors';
+
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf')
+  })
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded])
+
+
+  if (!loaded) {
+    return null
+  }
+
   return (
-    <View style={{ backgroundColor: 'white', flex: 1 }}>
+    <View style={{ backgroundColor: Colors.background, flex: 1 }}>
       <Slot />
       <StatusBar />
     </View>
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Colors } from '@/constants/Colors'
 
 export default function CalculadoraApp() {
     return (
         <View>
-            <Text style={{ fontSize: 50 }}>Calculadora App</Text>
+            <Text style={{ fontSize: 40, fontFamily: 'SpaceMono', color: Colors.textPrimary }}>
+                Calculadora App
+            </Text>
+            <Text style={{ fontSize: 25, fontFamily: 'SpaceMono', color: Colors.textSecondary }}>
+                2666
+            </Text>
         </View>
     )
 }
